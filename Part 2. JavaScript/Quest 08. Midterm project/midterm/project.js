da31d4adb03ba6bacde5992ac10d41ce50620e44
Juyeon <jyhan@knowre.com>
2018-08-06 18:28:36 +0900

    add Dummy class

--- Part 2. JavaScript/Quest 08. Midterm project/midterm/project.js
+++ Part 2. JavaScript/Quest 08. Midterm project/midterm/project.js
@@ -1,3 +1,7 @@
+// 더 해볼 내용
+// * api 연동, CDN 등으로 가져와보기
+// * 사진이 바뀔 때 배경도 함께 바뀌기
+
 // let loadPictures = function() {
 //     let request = new XMLHttpRequest();
 //     var catUrl = 'http://thecatapi.com/api/images';
@@ -13,75 +17,116 @@
 // }
 // loadPictures();
 
+const pictureListCat = [
+    'cat_01.jpg',
+    'cat_02.jpg',
+    'cat_03.jpg',
+    'cat_04.jpg',
+    'cat_05.jpg',
+    'cat_06.jpg',
+    'cat_07.jpeg',
+    'cat_08.jpg',
+    'cat_09.jpg',
+    'cat_10.jpg',
+];
+
+const pictureListDog = [
+    'dog_01.jpeg',
+    'dog_02.jpg',
+    'dog_03.jpg',
+    'dog_04.jpg',
+    'dog_05.jpg',
+    'dog_06.jpg',
+    'dog_07.jpg',
+    'dog_08.jpg',
+]
+
+const pictureDummy = [
+    pictureListCat,
+    pictureListDog
+]
+
+function changeColor() {
+    const colorList = [
+        '#c9d2fc',
+        '#FC7E7E',
+        '#F2CC7B',
+        '#A2D8C0',
+        '#6490f0'
+    ];
+    const randomColor = colorList[Math.floor(Math.random() * colorList.length)];
+    return randomColor;
+}
+
+function makeRandom(param) {
+    Math.floor(Math.random() * param.length)
+}
+
 class Window {
     constructor() {
-        const changeBgBtn = document.querySelector('.newColor');
-        changeBgBtn.onclick = this.changeBackground;
+        this.changeBgBtn = document.querySelector('.newColor');
+        this.changeBgBtn.addEventListener('click', this.changeBackground);
     }
     
     initialize() {
-        const picture = new Picture();
         const text = new Text();
         const time = new Time();
+        const dummy = new Dummy();
+
         time.initializeTime();
+        dummy.initializeDummy();
     }
     
     changeBackground() {
         const background = document.querySelector('body');
-        const bgColorList = [
-            '#c9d2fc',
-            '#FC7E7E',
-            '#F2CC7B',
-            '#A2D8C0',
-            '#6490f0'
-        ];
+        background.setAttribute('style', `background-color: ${changeColor()}`);
+    }
+}
 
-        const randomBackground = bgColorList[Math.floor(Math.random() * bgColorList.length)];
-        background.setAttribute('style', `background-color: ${randomBackground}`);
+class Dummy {
+    constructor() {
+        this.newPictureBtn = document.querySelector('.newPictures');
+        this.newPictureBtn.addEventListener('click', this.changeDummy.bind(this));
+        this.pickedDummy = pictureDummy[Math.floor(Math.random() * pictureDummy.length)];
+    }
+
+    initializeDummy() {
+        const picture = new Picture();
+        picture.initializePicture(this.pickedDummy);
+    }
+
+    changeDummy() {
+        this.initializeDummy();
     }
 }
 
 class Picture {
     constructor() {
-        const picture = document.querySelector('.picture-frame');
-        picture.onclick = this.changePicture;
+        this.picture = document.querySelector('.picture-frame');
+        this.picture.addEventListener('click', this.changePicture.bind(this));
+        this.dummy = pictureDummy[Math.floor(Math.random() * pictureDummy.length)]
+    }
+
+    initializePicture(dummy) {
+        const pickedPicture = dummy[Math.floor(Math.random() * dummy.length)];
+        this.picture.setAttribute('style', `background-image: url('./assets/${pickedPicture}')`)
     }
     
     changePicture() {
-        const pictureListCat = [
-            'cat_01.jpg',
-            'cat_02.jpg',
-            'cat_03.jpg',
-            'cat_04.jpg',
-            'cat_05.jpg',
-            'cat_06.jpg',
-            'cat_07.jpeg',
-            'cat_08.jpg',
-            'cat_09.jpg',
-            'cat_10.jpg',
-        ];
-
-        const pickedPicture = pictureListCat[Math.floor(Math.random() * 10)];
-        this.setAttribute('style', `background-image: url('./assets/${pickedPicture}')`)
-    }
-    
-    changeDummy() {
-        const dummies = [
-        ]
-    }
-
-    initializePicture() {
+        const pickedDummy = this.dummy;
+        const pickedPicture = pickedDummy[Math.floor(Math.random() * pickedDummy.length)];
+        this.picture.setAttribute('style', `background-image: url('./assets/${pickedPicture}')`)
     }
 }
 
 class Text {
     constructor() {
-        const greetingBtn = document.querySelector('.newGreetings');
-        greetingBtn.onclick = this.changeText;
+        this.greetingBtn = document.querySelector('.newGreetings');
+        this.greetingBtn.addEventListener('click', this.changeText.bind(this));
+        this.mainText = document.querySelector('h1');
     }
 
     changeText() {
-        const mainText = document.querySelector('h1');
         const textList = [
             'Welcome!',
             '패스트파이브가 싫어요',
@@ -91,29 +136,50 @@ class Text {
         ];
 
         const randomText = textList[Math.floor(Math.random() * textList.length)];
-        mainText.classList.add('blink');
-        mainText.textContent = randomText;
+        this.mainText.textContent = randomText;
+        this.mainText.setAttribute('style', `color: ${changeColor()}`);
     }
 }
 
 class Time {
     constructor() {
-        const timeBox = document.querySelector('.time-box');
-        this.year = document.getElementById('year');
-        this.month = document.getElementById('month');
-        this.date = document.getElementById('date');
-        this.hour = document.getElementById('hour');
-        this.minute = document.getElementById('minute');
-        this.second = document.getElementById('second');
+        this.timeBox = document.querySelector('.time-box');
+        this.today = this.timeBox.querySelector('.date');
+        this.timeNow = this.timeBox.querySelector('.time');
+        this.setTime = setInterval(this.initializeTime.bind(this), 1000);
     }
-
+    
     initializeTime() {
-        var time = new Date();
-        this.year.textContent = time.getFullYear() + '.';
-        this.month.textContent = time.getMonth() + 1  + '.';
-        this.date.textContent = time.getDate()  + '.';
-        this.hour.textContent = time.getHours() + '.';
-        this.minute.textContent = time.getMinutes() + '.';
-        // this.second.textContent = time.getSeconds();
+        const now = new Date();
+
+        const year = now.getFullYear();
+        const month = now.getMonth() + 1;
+        const date = now.getDate();
+        let hour = now.getHours();
+        let minute = now.getMinutes();
+        let seconds = now.getSeconds();
+        let session = "오전";
+        
+        if (hour == 0) {
+            hour = 12;
+        } else if (hour > 12) {
+            hour = hour - 12;
+            session = "오후";
+        } else return;
+        
+        hour = (hour < 10) ? "0" + hour : hour;
+        minute = (minute < 10) ? "0" + minute : minute;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+        
+        let timeNow = session + " " + hour + ":" + minute + ":" + seconds
+        let today = year + '년 ' + month + '월 ' + date + '일';
+
+        this.today.textContent = today;
+        this.timeNow.textContent = timeNow;
+        this.setTime;
     }
+    
+    // setTime() {
+    //     setInterval(this.initializeTime.bind(this), 1000);
+    // }
 }
