fd131b733c309611c595589814db89acbe421be2
Juyeon <jyhan@knowre.com>
2018-08-06 11:53:20 +0900

    refactor oop

--- Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
@@ -28,35 +28,34 @@ class Desktop {
 		this._folder = folderNum;
 		this.section = document.querySelector('section');
 	}
-
-	makeRandom(param) {
-		return Math.floor(Math.random() * param);
-	};
 		
 	initializeScreen() {
-		var icons = document.querySelectorAll('.icon'); // icon은 두개야..
+		const icons = document.querySelectorAll('.icon');
+		const desktop = document.querySelector('.desktop');
 		icons.forEach(el => {
-			return new Icon(makeRandom(document.body.clientWidth), makeRandom(document.body.clientHeight))
+			const icon = new Icon(desktop)
+			return icon.initializeIcon();
 		});
-		// icons.forEach(el => {
-		// 	return el.initializeIcon();
-		// })
 
 		var folder = document.querySelector('.folder');
-		folder = new Folder(makeRandom(document.body.clientWidth), makeRandom(document.body.clientHeight));
+		folder = new Folder(makeRandom(document.body.clientWidth), makeRandom(document.body.clientHeight), desktop);
 		folder.initializeFolder();
 	}
 };
 
 class Icon {
-	constructor(x, y) {
-		this._x = x;
-		this._y = y;
-		this.desktop = document.querySelector('.desktop');
+	constructor(desktop) {
+		this._x = makeRandom(document.body.clientWidth);
+		this._y = makeRandom(document.body.clientHeight);
+		this._desktop = desktop;
 	}
 	
+	makeRandom(param) {
+		return Math.floor(Math.random() * param);
+	};
+
 	initializeIcon() {
-		var icons = this.desktop.querySelectorAll('.icon');
+		var icons = this._desktop.querySelectorAll('.icon');
 		icons.forEach(el => {
 			el.style.top = this._x + 'px';
 			el.style.left = this._y + 'px';
@@ -66,32 +65,49 @@ class Icon {
 };
 
 class Folder extends Icon {
-	constructor(x, y) {
+	constructor(x, y, desktop) {
 		super();
 		this._x = x;
 		this._y = y;
+		this._desktop = desktop;
 	}
 
 	initializeFolder() {
-		console.log(this)
-		var folder = this.desktop.querySelector('.folder');
+		const folder = this._desktop.querySelector('.folder');
 		folder.style.top = this._x + 'px';
 		folder.style.left = this._y + 'px';
 		folder.addEventListener('mousedown', dragFunc);
+		folder.addEventListener('dblclick', this.openWindow);
+		// folder.ondblclick = this.openWindow(this._desktop); // desktop을 넘겨줄 수 있는 방법?
 	}
-
+	
 	openWindow() {
+		const window = new Window(makeRandom(document.body.clientWidth), makeRandom(document.body.clientHeight));
+		return window.initializeWindow();
 	}
-
+	
 };
 
 class Window {
-	constructor(x, y) {
+	constructor(x, y, desktop) {
 		this._x = x;
 		this._y = y;
+		// this._desktop = desktop;
+	}
+	
+	initializeWindow() {
+		const desktop = document.querySelector('.desktop');
+		const window = document.createElement('div');
+		window.setAttribute('class', 'window');
+		// this._desktop.append(window);
+		desktop.append(window);
+		window.textContent = 'This is window!';
+		// window.onmousedown = dragFunc; // addEventListener와 어떻게 다른 걸까?
+		window.addEventListener('mousedown', dragFunc);
+		window.ondblclick = this.closeWindow;
 	}
 
-	initializeWindow() {
-		
+	closeWindow() {
+		this.remove();
 	}
 };
