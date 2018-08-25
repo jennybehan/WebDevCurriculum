09585a5f66e478f8ae8b1eac7f7fcf760e28881e
Juyeon <jyhan@knowre.com>
2018-08-25 21:40:07 +0900

    refactor server

--- Part 3. node.js/Quest 13. Session and Login/skeleton/server.js
+++ Part 3. node.js/Quest 13. Session and Login/skeleton/server.js
@@ -4,23 +4,19 @@ const express = require('express'),
 	  bodyParser = require('body-parser'),
 	  fs = require('fs'),
 	  util = require('util'),
-	  session = require('express-session');
-
-// GET /memo list all contacts
-// POST /memo create new contact
-// GET /memo/{id} retrieve a single contact
-// PUT /memo/{id} update a single contact
+	  session = require('express-session'),
+	  cookieParser = require('cookie-parser');
 
 // middlewares
 app.use(express.static('client')); // app.use(express.static('public')) : 정적 파일을 사용하기 위한 설정
 app.use(bodyParser.json()) // parse requests of content-type - application/json
+app.use(cookieParser());
 
 app.use(session({
 	secret: 'secretkey', // session의 암호화에 사용되는 key값
 	name: 'sessionId',
 	resave: false, // request가 요청되었을때, 기존의 session이 존재하는 경우 다시 저장할 필요가 있는지를 확인하는 option
-	saveUninitialized: false, // 설정하지 않으면 세션이 저장되지 않음. 기본값은 true - 모든 초기화되지 않은 session이 저장.
-	// saveUninitialized: true,
+	saveUninitialized: true, // 설정하지 않으면 세션이 저장되지 않음. 기본값은 true - 모든 초기화되지 않은 session이 저장.
 	cookie: {
 		domain: 'http://localhost:8080/',
 		secure: true, // http로 호출되는 경우에만 session cookie를 생성하는 option. 	
@@ -44,9 +40,6 @@ const users = [
 ]
 
 app.get('/', (req, res) => {
-	// if(req.session.userName) {
-	// 	res.send(req.session.userName)
-	// }
 	res.sendFile(path.join(__dirname, 'index.html'));
 })
 
@@ -54,44 +47,33 @@ const findUser = (userId, userPw) => {
 	return users.find(user => user.userId === userId && user.userPw === userPw)
 }
 
+app.get('/login', (req, res) => {
+	const session = req.session;
+	console.log(session)
+	if (req.session) {
+		console.log(req.session)
+	}
+})
+
 app.post('/login', (req, res) => {
-	const body = req.body;
-	console.log(req.body)
-	// if (findUser(body.userId, body.userPw)) {
-	// 	console.log(req.session)
-	// 	res.set({ 'content-type': 'text/html; charset=utf-8' })
-	// 	res.redirect('/')
-	// } else {
-	// 	res.send('유효하지 않습니다. 다시 로그인해주세요.')
-	// }
+	const id = req.body.id;
+	const pw = req.body.pw;
+	console.log(id, pw)
+	if (findUser(id, pw)) {
+		console.log(req.session)
+		req.session.username = id;
+		req.session.login = true;
+		success = true;
+		res.redirect('/')
+	} else {
+		res.send('유효하지 않습니다. 다시 로그인해주세요.')
+	}
 });
 
 app.get('/logout', (req, res) => {
-	req.session.destroy(err => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.status(200).end();
-			res.redirect('/');
-		}
-	})
+	req.session.destroy();
+	res.redirect('/');
 })
-
-function writeMemo() {
-	return new Promise((resolve, reject) => {
-		fs.writeFile(pathName + fileName, {
-			encoding: 'utf8'
-		}, (err, fileText) => {
-			if (err) reject(err);
-			else resolve(
-				{
-					"title": fileName,
-					"body": fileText,
-				}
-			)
-		}
-	)}
-)}
 
 app.post('/memo', (req, res) => {
 	let data = req.body;
@@ -107,25 +89,26 @@ app.post('/memo', (req, res) => {
 })
 
 app.get('/memo/:fileName', (req, res) => {
+	console.log('test')
 	const fileName = req.params.fileName;
 	fs.readFile(pathName + fileName, 'utf-8', (err, data) => {
 		res.writeHead(200, {'Content-Type': 'application/json'});
 		const fileData = {
 			data
 		}
+		console.log(data)
 		res.end(JSON.stringify(fileData));
 	})
 })
 
 app.get('/memo', (req, res) => {
 	res.writeHead(200, {'Content-Type': 'application/json'});
-	//  getPost();
 	const readdirAsync = util.promisify(fs.readdir)
- 	const readFileAsync = filePath => new Promise((resolve, reject) => {
-		fs.readFile(filePath, (err, data) => {
-			data = data.toString();
+ 	const readFileAsync = title => new Promise((resolve, reject) => {
+		fs.readFile(title, (err, text) => {
+			text = text.toString();
 			if (err) reject(err);
-			resolve({filePath, data});
+			resolve({title, text});
 		})
 	})
 
