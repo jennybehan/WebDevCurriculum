c3c496cc36b822dd465906fb256200a855fce41a
Juyeon <jyhan@knowre.com>
2018-12-17 00:27:57 +0900

    add mysql in graphql project

--- Part 5. Web 2018/Quest 18. GraphQL/submit/notepad/server/index.js
+++ Part 5. Web 2018/Quest 18. GraphQL/submit/notepad/server/index.js
@@ -5,6 +5,7 @@ import {
     graphqlExpress,
     graphiqlExpress
 } from 'apollo-server-express';
+import mysql from 'mysql';
 import bodyParser from 'body-parser';
 import schema from './gql/schema';
 import resolvers from './gql/resolvers';
@@ -28,3 +29,67 @@ app.listen(GRAPHQL_PORT, () =>
         `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}`
     )
 );
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'gkswndus8162',
+    database: 'memo_db'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+});
+
+function getMemoList(resolve, reject) {
+    connection.query('SELECT * FROM memo', function (error, results, fields) {
+        if (error) return reject(error);
+        const resolvedResults = results.map(item => {
+            return {
+                id: item.id,
+                author: item.author,
+                title: item.title,
+                content: item.content,
+            }
+        })
+        resolve(resolvedResults);
+    });
+}
+
+const resolver = function (result) {
+    console.log(result);
+};
+const rejecter = function (err) {
+    console.log('에러');
+    console.error(err);
+}
+getMemoList(resolver, rejecter);
+
+// Promise
+
+function getMemoListPromise() {
+    return new Promise(getMemoList);
+}
+
+getMemoListPromise()
+    .then(resolver)
+    .catch(rejecter);
+
+async function getMemoListAsync() {
+    return await getMemoListPromise();
+}
+
+try {
+    getMemoListAsync();
+} catch (e) {
+    console.error(e);
+}
+
+// function getMemoList(callback) {
+//     connection.query('SELECT * FROM memo', callback);
+// }
+
+connection.end();
